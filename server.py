df761f2ec603c036687ad2e42b81e7d2f6e3fe2f
Manu <manuhazen@outlook.com>
2018-01-16 22:04:38 -0400

    Added the end points and worked with the mine

--- server.py
+++ server.py
@@ -1,1 +1,48 @@
-server.py
+from textwrap import dedent
+from time import time
+from uuid import uuid4
+
+from flask import Flask, jsonify, request
+
+from blockchain import Blockchain
+
+# Instantiate the node
+app = Flask(__name__)
+
+# Generate the address for this node
+node_identifier = str(uuid4()).replace('-','')
+
+# Instantiate the blockchain
+
+justchain = Blockchain()
+
+@app.route('/mine', methods=['GET'])
+def mine():
+	return 'Mining a new block'
+
+@app.route('/transactions/new', methods=['POST'])
+def new_transaction():
+	values = request.get_json()
+
+	# Checking if the required fields are in the POST request
+	required = ['sender', 'recipient', 'amount']
+	if not all(k in values for k in required):
+		return 'Missin values', 400
+
+	# Creating a new transaction if exists all the fields
+	index = justchain.new_transaction(values['sender'], values['recipient'], values['amount'])
+
+	response = {'message': f'Transaction will be added to Block {index}'}
+	return jsonify(response), 201
+
+@app.route('/chain', methods=['GET'])
+def full_chain():
+	response = {
+		'chain': justchain.chain,
+		'length': len(justchain.chain)
+	}
+	return jsonify(response), 200
+
+
+if __name__ == '__main__':
+	app.run(host='127.0.0.1', port=5000)
